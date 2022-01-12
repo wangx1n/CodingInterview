b0bb8bc0ad1506837b0fafa535111e9424999fa9
wangx1n <wangx1n9347@gmail.com>
2022-01-12 13:16:30 +0800

    add:每日一题 hard

--- goSrc/leetcode/test.go
+++ goSrc/leetcode/test.go
@@ -1,19 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"unsafe"
-)
+import "fmt"
 
 func main() {
-	var (
-		a []int
-		b = make([][]int, 3)
-		c = new([]int)
-	)
-	fmt.Println(a, b, c)
-	fmt.Println(unsafe.Sizeof(c))
-	fmt.Println(a, c)
-	fmt.Println(unsafe.Sizeof(a))
-	fmt.Println(unsafe.Sizeof(*c))
+	fmt.Println(3/10)
+	fmt.Println(30/10)
+	fmt.Println(14%10)
+	fmt.Println(3/10)
 }
